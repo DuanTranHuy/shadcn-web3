105316aa9fa29a9ad144736ed3d01d6573882c52
Tran Huy Duan <20841392+DuanTranHuy@users.noreply.github.com>
2025-12-10 20:59:17 +0700

    refactor: simplify AccountInfo component by removing unused variables and elements

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <DynamicWidget />
       <AccountInfo />
     </div>
--- components/AccountInfo.tsx
+++ components/AccountInfo.tsx
@@ -1,20 +1,12 @@
 "use client";
 import { useConnection } from "wagmi";
-import { BlockieAvatar } from "./web3/blockie-avatar";
 import { Address } from "./web3/address";
 
 export const AccountInfo = () => {
-  const { address, isConnected, chain } = useConnection();
+  const { address } = useConnection();
   return (
     <div>
-      <p>
-        wagmi connected: {isConnected ? 'true' : 'false'}
-      </p>
-      <p>wagmi address: {address}</p>
-      <p>wagmi network: {chain?.id}</p>
-      {address && <BlockieAvatar address={address}/>}
       {address && <Address address={address}/>}
-
     </div>
   );
 }
