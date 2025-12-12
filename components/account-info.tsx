0d4203db64f84f8318bb288c9632c8d8313a5fdb
Tran Huy Duan <20841392+DuanTranHuy@users.noreply.github.com>
2025-12-13 06:19:45 +0700

    refactor: remove unused NetworkOptions import from AccountInfo component

--- components/account-info.tsx
+++ components/account-info.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useAccount } from "wagmi";
 import { Address } from "./web3/address";
-import { NetworkOptions, NetworkSwitcher } from "./web3/network";
+import { NetworkSwitcher } from "./web3/network";
 import { Transfer } from "./web3/transaction";
 import { TARGET_NETWORKS } from "@/config/networks";
 
@@ -11,7 +11,6 @@ export const AccountInfo = () => {
     <div className="space-y-4">
       {address && <Address address={address}/>}
       {address && <NetworkSwitcher chains={TARGET_NETWORKS} />}
-      {address && <NetworkOptions chains={TARGET_NETWORKS} />}
       {address && <Transfer />}
     </div>
   );
